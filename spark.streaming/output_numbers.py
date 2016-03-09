f0793f45a4052a6d7fe89c14747ca068849c7f9b
Dan Serban <dserban01@yahoo.com>
2016-03-09 19:38:45 +0200

    Update output_numbers.py

--- spark.streaming/output_numbers.py
+++ spark.streaming/output_numbers.py
@@ -2,12 +2,10 @@ from sys    import stdout
 from time   import sleep
 from random import choice
 
-
 cycle_through = range(50)
 while True:
     for i in cycle_through:
-        sleep(0.3)
+        sleep(1.0)
         times_ten = i * 10
-        print choice( range(times_ten, times_ten + 200) )
+        print choice( range(times_ten, times_ten + 10) )
         stdout.flush()
-
